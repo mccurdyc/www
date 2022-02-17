5e3d3d215caa6c68fb160da487011811d8f780a4
Colton J. McCurdy <mccurdyc22@gmail.com>
2022-02-17 06:28:52 -0500

    full polyfill, still not working
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- static/js/read-gcs/webpack.config.js
+++ static/js/read-gcs/webpack.config.js
@@ -13,6 +13,25 @@ module.exports = {
       resourceRegExp: /worker_threads/,
     }),
   ],
+  // resolve: {
+  //   fallback: {
+  //     "assert": require.resolve("assert/"),
+  //     "buffer": require.resolve("buffer/"),
+  //     "constants": require.resolve("constants-browserify"),
+  //     "crypto": require.resolve("crypto-browserify"),
+  //     "http": require.resolve("stream-http"),
+  //     "https": require.resolve("https-browserify"),
+  //     "os": require.resolve("os-browserify/browser"),
+  //     "path": require.resolve("path-browserify"),
+  //     "querystring": require.resolve("querystring-es3"),
+  //     "stream": require.resolve("stream-browserify"),
+  //     "url": require.resolve("url/"),
+  //     "net": require.resolve("net"),
+  //     "tls": require.resolve("tls"),
+  //     "util": require.resolve("util/"),
+  //     "zlib": require.resolve("browserify-zlib"),
+  //   },
+  // },
   // externals: {
   //   '@google-cloud/storage': 'commonjs @google-cloud/storage'
   // },
