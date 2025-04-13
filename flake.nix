b281c57d34edb9069840024802dd72b2ee5770ef
Colton J. McCurdy <mccurdyc22@gmail.com>
2025-04-13 10:18:44 -0400

    update nix flake
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,17 +1,87 @@
 {
+  description = "Repo configuration";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells = {
-            default = import ./shell.nix { inherit pkgs; };
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = { };
+
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      # This is needed for pkgs-unstable - https://github.com/hercules-ci/flake-parts/discussions/105
+      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+
+      perSystem = { system, ... }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
           };
-        }
-      );
+          pkgs-unstable = import inputs.nixpkgs-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
+
+          ci_packages = {
+            # Nix
+            nix-fmt = pkgs.nixpkgs-fmt;
+          };
+
+          packages = (builtins.attrValues ci_packages) ++ [
+            pkgs-unstable.nil
+            pkgs.deadnix
+            pkgs.gnumake
+            pkgs.google-cloud-sdk
+            pkgs.hugo
+            pkgs.nixpkgs-fmt
+            pkgs.statix
+            pkgs.wget
+          ];
+        in
+        {
+          # This is needed for pkgs-unstable - https://github.com/hercules-ci/flake-parts/discussions/105
+          overlayAttrs = { inherit pkgs-unstable; };
+
+          formatter = pkgs.nixpkgs-fmt;
+
+          # https://github.com/cachix/git-hooks.nix
+          # 'nix flake check'
+          checks = {
+            pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                # Nix
+                deadnix.enable = true;
+                nixpkgs-fmt.enable = true;
+                statix.enable = true;
+
+                # Shell
+                shellcheck.enable = true;
+                shfmt = {
+                  enable = true;
+                  entry = "shfmt --simplify --indent 2";
+                };
+              };
+            };
+          };
+
+          packages = ci_packages;
+
+          devShells.default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+            inherit packages;
+          };
+        };
+    };
 }
