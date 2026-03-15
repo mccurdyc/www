cefcd5623b51e9d15a1eb9aa4ec5c4e7d122b9c3
Colton J. McCurdy <mccurdyc22@gmail.com>
2026-03-15 13:01:48 -0400

    nix flake update
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,13 +2,12 @@
   description = "Repo configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, flake-parts, ... }:
+  outputs = inputs@{ self, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       flake = { };
 
@@ -17,16 +16,9 @@
         "x86_64-linux"
       ];
 
-      # This is needed for pkgs-unstable - https://github.com/hercules-ci/flake-parts/discussions/105
-      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
-
       perSystem = { system, ... }:
         let
           pkgs = import inputs.nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-          pkgs-unstable = import inputs.nixpkgs-unstable {
             inherit system;
             config.allowUnfree = true;
           };
@@ -37,7 +29,7 @@
           };
 
           packages = (builtins.attrValues ci_packages) ++ [
-            pkgs-unstable.nil
+            pkgs.nil
             pkgs.deadnix
             pkgs.gnumake
             pkgs.google-cloud-sdk
@@ -48,8 +40,6 @@
           ];
         in
         {
-          # This is needed for pkgs-unstable - https://github.com/hercules-ci/flake-parts/discussions/105
-          overlayAttrs = { inherit pkgs-unstable; };
 
           formatter = pkgs.nixpkgs-fmt;
 
